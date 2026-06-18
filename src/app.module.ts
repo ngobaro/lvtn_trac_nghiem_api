36a943f520ca5df79630a7684a4fbf911887f3c7
ngobaro <ngobaocoder@gmail.com>
2026-06-18 15:32:14 +0700

    chore(config): thêm cấu hình app, database, jwt, mail và google oauth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
+import mailConfig from './config/mail.config';
+import googleOauthConfig from './config/google-oauth.config';
+import appConfig from './config/app.config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+       load: [
+        databaseConfig,
+        jwtConfig,
+        mailConfig,
+        googleOauthConfig,
+        appConfig,
+      ],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
