0a463a59d8fbd179f0396889eae9f8a63d2ccdee
ngobaro <ngobaocoder@gmail.com>
2026-06-19 17:41:29 +0700

    Cập nhật cấu hình cơ sở dữ liệu để sử dụng TypeOrmModuleOptions và thêm các tùy chọn kết nối

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,13 @@
-import { registerAs } from "@nestjs/config";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export default registerAs('database', () => ({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-}));
+export const databaseConfig = (): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 4000,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  autoLoadEntities: true,
+  synchronize: false,
+  ssl: process.env.DB_SSL_CA ? { ca: process.env.DB_SSL_CA } : undefined,
+});
